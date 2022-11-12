10f92c4d0ab435504ca83bdc8126ca7e9130e2a5
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-11-12 04:33:44 +0300

    Add AddSetter quick fix

--- ReSharper.FSharp/src/FSharp.Psi.Daemon/src/Stages/FcsErrorsStageProcessBase.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Daemon/src/Stages/FcsErrorsStageProcessBase.fs
@@ -75,6 +75,7 @@ module FSharpErrors =
     let [<Literal>] FieldRequiresAssignment = 764
     let [<Literal>] EmptyRecordInvalid = 789
     let [<Literal>] PropertyIsStatic = 809
+    let [<Literal>] PropertyCannotBeSet = 810
     let [<Literal>] LocalClassBindingsCannotBeInline = 894
     let [<Literal>] TypeAbbreviationsCannotHaveAugmentations = 964
     let [<Literal>] UnusedValue = 1182
@@ -379,6 +380,9 @@ type FcsErrorsStageProcessBase(fsFile, daemonProcess) =
 
         | PropertyIsStatic ->
             createHighlightingFromNode PropertyIsStaticError range
+
+        | PropertyCannotBeSet ->
+            createHighlightingFromNode PropertyCannotBeSetError range
 
         | LocalClassBindingsCannotBeInline ->
             createHighlightingFromParentNode LocalClassBindingsCannotBeInlineError range
